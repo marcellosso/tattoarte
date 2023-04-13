8ecd7d1bfcc6afdf78ed51bf722b5ea5d7ca4272
marcellosso <marcel.losso@gmail.com>
2023-04-13 19:37:39 -0300

    Adding app page functionalities

---
+++ components/app-navbar.tsx
@@ -0,0 +1,109 @@
+import processPayment from '@/utils/payment';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Link from 'next/link';
+import React, { FC, useMemo } from 'react';
+
+const AppNavbar = () => {
+  const { user } = useUser();
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const userInitials = useMemo(() => {
+    const name = user?.name || '';
+    const initials = name
+      .match(/(^\S\S?|\b\S)?/g)
+      ?.join('')
+      ?.match(/(^\S|\S$)?/g)
+      ?.join('')
+      .toUpperCase();
+
+    return initials;
+  }, [user?.name]);
+
+  return (
+    <nav className="bg-secondary fixed w-full z-20 top-0 left-0 shadow-lg max-h-16">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
+        <Link href="/" className="flex items-center">
+          {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo"> */}
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            TattoArte
+          </span>
+        </Link>
+        <div className="flex md:order-2 items-center">
+          <button
+            onClick={() => processPayment('price_1MwG5IICcQQfNZPtAjiwGx5J')}
+            type="button"
+            className="bg-gradient-to-r font-bold text-letter p-3 rounded-md from-green-600 to-blue-700 hover:from-pink-500 hover:to-yellow-500 ..."
+          >
+            Compre o passe de acesso
+          </button>
+          <button
+            data-toggle="collapse"
+            data-target="#navbar-sticky"
+            type="button"
+            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Abra o menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+
+          <div>
+            <div
+              id="userAvatar"
+              onClick={() => setIsOpen(!isOpen)}
+              className="relative inline-flex items-center justify-center ml-3 w-12 h-12 overflow-hidde rounded-full bg-gray-600 hover:bg-gray-700 hover:cursor-pointer"
+            >
+              <span className="font-medium text-gray-600 dark:text-gray-300">
+                {userInitials}
+              </span>
+            </div>
+
+            <div
+              id="userDropdown"
+              className={`z-10 divide-y absolute mt-3 rounded-md shadow w-44 dark:bg-gray-700 ${
+                isOpen ? 'block' : 'hidden'
+              }`}
+            >
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="userAvatar"
+              >
+                <li>
+                  <Link
+                    href="#"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Compras
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="#"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Sair
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default AppNavbar;
--- pages/app.tsx
+++ pages/app.tsx
@@ -1,3 +1,5 @@
+import tattooStyles from '@/assets/tattoo-styles';
+import AppNavbar from '@/components/app-navbar';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { UserProfile } from '@auth0/nextjs-auth0/client';
 import React, { FC } from 'react';
@@ -7,7 +9,92 @@ interface IAPP {
 }
 
 const App: FC<IAPP> = ({ user }) => {
-  return <div>This is the App page {user.name} </div>;
+  return (
+    <>
+      <AppNavbar />
+      <main className="flex min-h-screen h-screen flex-col items-center justify-between pt-16 bg-primary text-letter">
+        <section className="flex w-screen h-full">
+          <div className="bg-secondary w-1/5 h-full shadow-lg shadow-gray-400">
+            <h1 className="text-xl font-bold text-letter border-b-2 text-center divide-letter py-2">
+              Crie suas <span className="text-detail">tattoos</span>,{' '}
+              {user.name}!
+            </h1>
+
+            <div className="p-4">
+              <div className="mb-3">
+                <label
+                  htmlFor="message"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Descreva sua tattoo
+                </label>
+                <textarea
+                  id="message"
+                  rows={4}
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Um pescador viajando pelo espaço"
+                />
+              </div>
+
+              <div className="mb-3">
+                <label
+                  htmlFor="cores"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Cores
+                </label>
+                <select
+                  id="countries"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option value="Preto e Branco">Preto e Branco</option>
+                  <option value="Colorido">Colorido</option>
+                </select>
+              </div>
+
+              <div className="mb-3">
+                <label
+                  htmlFor="estilo"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Estilo
+                </label>
+                <select
+                  id="estilo"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  {tattooStyles.map((tattoo, idx) => (
+                    <option key={idx} value={tattoo}>
+                      {tattoo}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="mb-3">
+                <label
+                  htmlFor="first_name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Artistas para inspiração
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Tarsila do Amaral, Cândido Portinari, Romero Britto"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div className="w-4/5 p-4 bg-primary">
+            <div className="bg-secondary h-full w-full rounded-lg shadow-md shadow-gray-400"></div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export const getServerSideProps = withPageAuthRequired();
