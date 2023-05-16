e68ce18739093cd511335cdefa3f2b509e2a8733
marcellosso <marcel.losso@gmail.com>
2023-05-15 23:39:50 -0300

    Changed user id again

--- pages/api/auth/hooks.ts
+++ pages/api/auth/hooks.ts
@@ -14,8 +14,9 @@ module.exports = async (req: any, res: any) => {
         email,
       });
 
+      const userId = id.split('|')[1];
       const user = await prisma.user.create({
-        data: { id, email, name, stripeId: customer.id, credits: 1 },
+        data: { id: userId, email, name, stripeId: customer.id, credits: 1 },
       });
 
       res.status(200).json(user.id);
